0384f6b79a820eaeb168cb96e7debb49f518dd2c
Ludo van den Boom <ludo@cubicphuse.nl>
2013-09-28 11:59:27 +0200

    StringsAndBinaries-5

---
+++ strings/formatter.exs
@@ -0,0 +1,21 @@
+defmodule Formatter do
+  # Exercise: StringsAndBinaries-5
+  #
+  # Formatter.center(["cat", "zebra", "elephant"])
+  #   cat
+  #  zebra
+  # elephant
+  def center(words) do
+    column_width = Enum.max_by(words, &(String.length(&1))) |> String.length
+    print Enum.map(words, &(center_word &1, column_width))
+  end
+
+  def center_word(word, column_width) do
+    length = String.length word
+    String.rjust word, round((column_width - length) / 2 + length)
+  end
+
+  def print(words) do
+    Enum.each words, &(IO.puts &1)
+  end
+end
